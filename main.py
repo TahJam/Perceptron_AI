6b62866d4f62afde475ea2c3cffb642ee64f5d8c
Taher Jamali <72470092+rehat1@users.noreply.github.com>
2022-08-16 13:20:25 -0700

    Edited comments

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from perceptron import *
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     weights = [-0.5, 0, 0.5, 0, -0.5]
     examples = [
         [True, [1, 1, 1, 1, 0]],
@@ -28,5 +18,3 @@ if __name__ == '__main__':
 
     result = perceptron(0.5, 0.1, weights, examples, 4)
     printOutput(result, 4)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
